d2dce1ad91d2ff731e2fca8d2eeb366cba0befb3
skosarew <skosarew@mail.ru>
2020-01-29 01:24:49 +0300

    css fix

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS",
-                               '127.0.0.1 0.0.0.0 [::1]').split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
